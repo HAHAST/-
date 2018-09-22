96e8db399ce850c7d15aa695a01e49a1e0a223d9
HAHAST <36301573+HAHAST@users.noreply.github.com>
2018-09-22 17:19:42 +0800

    Update PID.c

--- PID.c
+++ PID.c
@@ -1,1 +1,27 @@
 
+//PID算法
+float PID(float set, float actual, PIDparameter *haha){
+    haha->err2 = haha->err1;
+    haha->err1 = haha->err;
+    
+    haha->err = set - actual;
+    haha->errint += haha->err;
+	
+    return haha->kp*haha->err + haha->ki*haha->errint + haha->kd*(3 * haha->err - 4 * haha->err1 + haha->err2);
+}
+
+//三个方向都算一遍内外环PID  (ง •_•)ง
+void pid_motor(float *pid_out, float *set, float *actual, PIDparameter *(pid_value[])){
+    unsigned char i=0;
+    float w_test[3] = {0,0,0};
+    
+    for (i=0; i<3; i++){
+        pid_out[i] = PID(PID(set[i], actual[i], pid_value[i]),
+                         w_test[i], pid_value[i+3]);
+    }
+}
+
+
+
+
+
